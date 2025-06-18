d32347e57719efd5de43e2b0968ca33b3d44df63
Phatt02 <Phattrapha02@gmail.com>
2025-06-18 11:32:30 +0700

    First commit

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,8 @@ class Config:
     SECRET_KEY = 'your-secret-key'
     JWT_SECRET_KEY = 'your-jwt-secret'
     MONGODB_SETTINGS = {
-        'db': 'mario_notes',
-        'host': 'localhost',
+        'db': 'mario_notes02',#ยังไม่ได้ใช้
+        'host': 'localhost02',
         'port': 27017
     }
+    # nannapas
